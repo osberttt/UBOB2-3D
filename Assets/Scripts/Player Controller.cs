b476e0b1b0808e8f7cf33a6f0bb8a7242845ff4a
osbert <minhtetnaing25mhn@gmail.com>
2025-12-04 19:08:06 +0700

    New Input System

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -1,7 +1,32 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputActionAsset inputActionAsset;
+
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private Vector2 move;
+
+    private void OnEnable()
+    {
+        inputActionAsset.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActionAsset.FindActionMap("Player").Disable();
+    }
+
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
     public float speed = 5f;
     private Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -13,10 +38,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
-        var move = new Vector3(horizontal, 0, vertical);
+        move = moveAction.ReadValue<Vector2>();
         move.Normalize();
-        rb.linearVelocity = new Vector3(move.x * speed, rb.linearVelocity.y, move.z * speed);
+        rb.linearVelocity = new Vector3(move.x * speed, rb.linearVelocity.y, move.y * speed);
+
+        if (jumpAction.WasPressedThisFrame()) // Input.GetButtonDown("Jump")
+        {
+            Debug.Log("Jump");
+        }
+        
     }
 }
